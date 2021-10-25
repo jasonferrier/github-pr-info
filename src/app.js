3dda5f174373858beab8c0a63a70c4ca89ada0d1
Jason Ferrier <jason@outerspatial.com>
2021-10-24 21:50:51 -0600

    Refactored to use node-fetch (vs built-in HTTPS); Implemented API request throttling to avoid Github secondary rate limiting; Implemented pagination on all requests;

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express'
-import { openPullRequests } from './openPullRequests.js'
+import { openPullRequestsInfo } from './openPullRequests.js'
 
 const app = express()
-const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('This is the root of the server. Hit the `/open-pull-requests` endpoint for Github PR info.')
 })
 
 app.get('/open-pull-requests', (req, res) => {
-  openPullRequests(req, res)
+  openPullRequestsInfo(req, res)
 })
 
 export default app
--- src/openPullRequests.js
+++ src/openPullRequests.js
@@ -1,4 +1,16 @@
-import https from 'https'
+import fetch from 'node-fetch'
+import createThrottle from 'async-throttle'
+const throttle = createThrottle(5)
+
+const apiBase = 'https://api.github.com'
+const fetchOptions = {
+  method: 'GET',
+  headers: {
+    authorization: `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    accept: 'application/vnd.github.v3+json',
+    'User-Agent': 'jasonferrier/github-pr-info'
+  }
+}
 
 const getUserAndRepo = (url) => {
   // This assumes a valid github URL in the format https://github.com/USERNAME/REPOSITORY
@@ -11,67 +23,85 @@ const getUserAndRepo = (url) => {
   }
 }
 
-export const openPullRequests = (req, res) => {
-  const clientResponse = res
+const getNextFromLink = (link) => {
+  const decodedHeader = link ?
+    Object.fromEntries(
+      link.split(/\s*,\s*/).map(r => {
+        const m = r.match(/<([^>]+)>;\s*rel="([^\"]+)"/);
+        return [m[2], m[1]];
+      })
+    ) : {}
+
+  return decodedHeader['next']
+}
+
+const fetchPaginatedData = async(url, options) => {
+  let next = url;
+  let results = []
+
+  do {
+    const response = await fetch(next, options)
+    // https://docs.github.com/en/rest/guides/traversing-with-pagination
+    const link = await response.headers.get('link')
+    const json = await response.json()
+    if (json.message && json.message === 'Not Found') {
+      results = [{error: 'There was an error retrieving your data.'}]
+      next = null
+    } else {
+      results = results.concat(json)
+      next = getNextFromLink(link)
+    }
+
+  } while (next)
+
+  return results
+}
+
+export const openPullRequestsInfo = async (req, res) => {
   const repositoryUrl = req.query.repositoryUrl
   const repoInfo = getUserAndRepo(repositoryUrl)
-  const options = {
-    hostname: 'api.github.com',
-    port: 443,
-    headers: {
-      authorization: `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      accept: 'application/vnd.github.v3+json',
-      'User-Agent': 'jasonferrier/github-pr-info'
-    },
-    path: `/repos/${repoInfo.owner}/${repoInfo.repo}/pulls`
-    // Useful query string parameters:
-    //    state: 'open', // default is open
-    //    per_page: 30, // Default 30
-    //    page: 1 // default 1
+
+  // const pullRequests = await fetchPaginatedData(`${apiBase}/repos/${repoInfo.owner}/${repoInfo.repo}/pulls?state=closed`, fetchOptions)
+  const pullRequests = await fetchPaginatedData(`${apiBase}/repos/${repoInfo.owner}/${repoInfo.repo}/pulls`, fetchOptions)
+
+  if (pullRequests.length === 0) {
+    res.json({message: 'No open pull requests found.'})
+  } else if (pullRequests.length === 1 && pullRequests[0].error) {
+    res.json(pullRequests[0])
+    return
+  } else {
+    const pullIDs = pullRequests.map(pr => pr.number)
+    let numCommitsOnEachPR = []
+
+    // Deal Github secondary rate limiting https://docs.github.com/en/rest/guides/best-practices-for-integrators#dealing-with-secondary-rate-limits
+    Promise.all(pullIDs.map((pullNumber) => throttle(async () => {
+      await fetchPaginatedData(`${apiBase}/repos/${repoInfo.owner}/${repoInfo.repo}/pulls/${pullNumber}/commits`, fetchOptions)
+        .then(commits => {
+          numCommitsOnEachPR.push({pullNumber: pullNumber, numCommits: commits.length})
+        })
+    })))
+      .then(() => {
+        // Provide response back with filtered PR information and the number of commits associated to each
+        const filteredResults = pullRequests.map(pr => {
+          let numCommits = 0
+          const numCommitsResults = numCommitsOnEachPR.filter(nc => nc.pullNumber === pr.number)
+          if (!!numCommitsResults.length) {
+            numCommits = numCommitsResults[0].numCommits
+          }
+
+          return {
+            number: pr.number,
+            title: pr.title,
+            body: pr.body,
+            commits: numCommits,
+            commits_url: pr.commits_url,
+            url: pr.html_url,
+            created_at: pr.created_at,
+            updated_at: pr.updated_at
+          }
+        })
+
+        res.json(filteredResults)
+      })
   }
-  
-  https.get(options, res => {
-    const { statusCode } = res
-    const contentType = res.headers['content-type']
-    const link = res.headers['link']
-    // If there are more than `per_page` results, the response will contain the Link header
-    // https://docs.github.com/en/rest/guides/traversing-with-pagination
-
-    if (link) {
-      // TODO: implement pagination with subsequent queries to append the resulting data.
-      console.log('There is pagination to deal with.')
-    }
-
-    let error
-    // Any 2xx status code signals a successful response but
-    // here we're only checking for 200.
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`)
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`)
-    }
-    if (error) {
-      console.error(error.message)
-      // Consume response data to free up memory
-      res.resume()
-      return
-    }
-
-    res.setEncoding('utf8')
-    let rawData = ''
-    res.on('data', (chunk) => { rawData += chunk })
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData)
-        // console.log(parsedData)
-        clientResponse.json(parsedData)
-      } catch (e) {
-        console.error(e.message)
-      }
-    })
-  }).on('error', (e) => {
-    console.error(`Got error: ${e.message}`)
-  })
 }
